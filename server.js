e90162e342ad93430e6bf3329a929965dca02b5c
phoenixp123 <ppagan@clarku.edu>
2022-05-09 17:45:30 -0400

    index page complete

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const session = require('express-session');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000, function () {
+    console.log("server started at 3000");
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get('/support', function (req, res) {
+    res.sendFile(__dirname + "/public/support.html");
+});
